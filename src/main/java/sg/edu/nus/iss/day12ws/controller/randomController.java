c1cae38668a003eca1da86aa815f84f6f151edee
imshamus <shamus-sjh@hotmail.com>
2024-11-20 00:47:19 +0800

    completed WS12

--- src/main/java/sg/edu/nus/iss/day12ws/controller/randomController.java
+++ src/main/java/sg/edu/nus/iss/day12ws/controller/randomController.java
@@ -1,6 +1,8 @@
 package sg.edu.nus.iss.day12ws.controller;
 
+import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,23 +11,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/randomEntry")
+@RequestMapping("/form")
 public class randomController 
 {
+    // Show blank form
     @GetMapping(path = "")
     public String randomEntry()
     {
-        return "randomEntry"; // returns random.html
+        return "form"; // returns random.html
     }
 
-    @GetMapping(path = "/generate")
-    public String randomGenerator(@RequestParam("Number") int number, Model model)
+    @GetMapping(path = "/generated")
+    public String randomGenerator(@RequestParam("number") int number, Model model)
     {
+        Set<Integer> randomNumbers = new LinkedHashSet<>();
         Random rand = new Random();
-        int randInt = rand.nextInt(1,31); // 1 is inclusive, 31 is exclusive
 
-        model.addAttribute("randomNumber", randInt);
-        return "randomGenerated";
+        while (randomNumbers.size() < number)
+        {
+            randomNumbers.add(rand.nextInt(1,21)); // 1 is inclusive, 21 is exclusive
+        }
+
+        // Pass numbers to the model
+        model.addAttribute("numbers", randomNumbers);
+        return "results";
         
     }
 }
